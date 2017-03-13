8874537b8fdd7ca4a02e636541136cfddc15899b
norahnn <norah15@mit.edu>
2017-03-13 10:18:15 -0400

    'testing'

---
+++ norahnn/exercise.js
@@ -0,0 +1,209 @@
+
+var exercise = {};
+
+
+
+exercise.hello = function(){
+    // ----------------------------------------
+    //   QUESTION 01
+    //   Return a string that says "Hello!"
+    // ----------------------------------------
+    return "Hello!";
+};
+
+exercise.numberArray = function(){
+    // ----------------------------------------
+    //   QUESTION 02
+    //   Return an array of numbers
+    // ----------------------------------------
+    return 1232134123
+};
+
+exercise.nameAndAge = function(){
+    // ----------------------------------------
+    //   QUESTION 03
+    //   Return an object with
+    //   a 'name' and 'age' property
+    // ----------------------------------------
+    return {'name':"Norah", 'age':100};
+};
+
+exercise.objectArray = function(){
+    // ----------------------------------------
+    //   QUESTION 04
+    //   Return an array of empty objects
+    //   The array can have any length you like.
+    // ----------------------------------------
+    return [[],[]];
+};
+
+exercise.addProperty = function(someObject){
+    // ----------------------------------------
+    //   QUESTION 05
+    //   Add the property 'name' to someObject
+    //   Give 'name' any value you like.
+    // ----------------------------------------
+    return someObject + "13";
+};
+
+exercise.thirdElement = function(someArray){
+    // ----------------------------------------
+    //   QUESTION 06
+    //   Return the third element of array
+    // ----------------------------------------
+    return someArray[2];
+};
+
+exercise.concatenate = function(string1, string2, string3){
+    // ----------------------------------------
+    //   QUESTION 07
+    //   Return the concatenation of
+    //   string1, string2, string3
+    // ----------------------------------------
+    return string1+string2+string3
+};
+
+exercise.greaterThanSix = function(someNumbers){
+    // ----------------------------------------
+    //   QUESTION 08
+    //   Filter array for values greater than 6
+    //   Return the filtered array
+    // ----------------------------------------
+    function isGreaterThan6(number){
+        return number>6;
+    }
+    return someNumbers.filter(isGreaterThan6);
+    
+};
+
+exercise.getNames = function(people){
+    // ----------------------------------------
+    //   QUESTION 09
+    //   Return a new array with
+    //   the "name" property values.
+    //
+    //   Example, for:
+    //   people = [
+    //         {name:'peter', age:18},
+    //         {name:'bruce', age:22},
+    //         {name:'clark', age:25},
+    //         {name:'diana', age:29}
+    //   ];
+    //
+    //   You should return
+    //     ['peter','bruce','clark','diana']
+    // ----------------------------------------
+    output=[];
+    for( var i=0;i<people.length;i++){
+        output.push(people[i].name);
+
+    }
+    return output;
+};
+
+exercise.getSalaryTotal = function(people){
+    // ----------------------------------------
+    //   QUESTION 10
+    //   Return the sum of all the
+    //   salaries in array.
+    //
+    //   Example, for:
+    //   people = [
+    //         {name:'peter', salary:1000},
+    //         {name:'bruce', salary:2000},
+    //         {name:'clark', salary:3000},
+    //         {name:'diana', salary:4000}
+    //     ];
+    //
+    //   you should return
+    //     10000
+    // ----------------------------------------
+    output=0;
+    for( var i=0;i<people.length;i++){
+        output+=people[i].salary;
+
+    }
+    return output;
+};
+
+exercise.sortNumbers = function(someNumbers){
+    
+    // ----------------------------------------
+    //   QUESTION 11
+    //   Sort numbers in array,
+    //   return the sorted array
+    //   Ascending or descenging.
+    // ----------------------------------------
+    
+};
+
+exercise.executeCallback = function(callback){
+    // ----------------------------------------
+    //   QUESTION 12
+    //   Execute callback
+    // ----------------------------------------
+    return 'Error: Question 12 not implemented';
+};
+
+exercise.getPersonAndJob = function(payroll){
+    var output=[];
+    for( var i=0;i<payroll.length;i++){
+        temp=[];
+        temp.push(payroll[i][1]);
+        temp.push(payroll[i][2]);
+        output.push(temp);
+    }
+    return output;
+
+    // ----------------------------------------
+    //   QUESTION 13
+    //   Return an array of people and jobs
+    //  [['person', 'job'], ['person,job'];
+    // ----------------------------------------
+
+};
+
+exercise.getTotalPayroll = function(payroll){
+    // ----------------------------------------
+    //   QUESTION 14
+    //   Return the total payroll
+    //   as an integer, use reduce
+    // ----------------------------------------
+    var payrolls = payroll.map(function getPayRolls(person){
+        return Number(person[3]);
+    });
+    function add(num1,num2){
+            var output =num1+num2;
+            return output;
+        }
+    return Math.floor(payrolls.reduce(add,0));
+};
+
+exercise.getEarningsAbove = function(payroll,target){
+    // ----------------------------------------
+    //   QUESTION 15
+    //   Return the number of people with
+    //   earnings above target
+    // ----------------------------------------
+    function aboveTarget(earning){
+        return earning>target;
+    }
+    var payrolls = payroll.map(function getPayRolls(person){
+        return Number(person[3]);
+    });
+    var filtered=payrolls.filter(aboveTarget);
+
+    return filtered.length;
+};
+
+exercise.getNumberOfZipCodesWith = function(payroll,num){
+    // ----------------------------------------
+    //   QUESTION 16
+    //   Return the number of zip codes
+    //   with 'num' anywhere in them
+    // ----------------------------------------
+    return 'Error: Question 16 not implemented';
+};
+
+
+module.exports = exercise;
